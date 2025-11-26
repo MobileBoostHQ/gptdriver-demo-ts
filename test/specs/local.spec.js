6aac43ab80739c6bcb43c69603b01b5bad815c6a
Chris <cs@mobileboost.io>
2025-11-26 11:49:35 +0100

    Initial commit

---
+++ test/specs/local.spec.js
@@ -0,0 +1,22 @@
+import GptDriver from "gpt-driver-node";
+
+describe("Test spec example", () => {
+    let gptDriver;
+
+    beforeEach(async function () {
+        const baseUrl = `${browser.options.protocol}://${browser.options.hostname}:${browser.options.port}${browser.options.path}`;
+        gptDriver = new GptDriver({
+            apiKey: browser.options.gptDriverApiKey,
+            driver: browser,
+            serverConfig: {
+                url: baseUrl
+            },
+            cachingMode: "FULL_SCREEN", // options: "INTERACTION_REGION", "FULL_SCREEN", "NONE"
+            testId: (this.currentTest?.parent?.title || 'unknown-suite') + ' - ' + (this.currentTest?.title || 'unknown-test')
+        });
+    })
+
+    it("open the youtube app", async () => {
+        await gptDriver.execute("open the youtube app");
+    });
+});
---
+++ wdio.conf.js
@@ -0,0 +1,153 @@
+import allure from "@wdio/allure-reporter"
+
+const elementTimeout = 10000;
+
+export const config = {
+    protocol: 'http',
+    hostname: '127.0.0.1',
+    port: 4723,
+    path: '/',
+    //
+    // ====================
+    // Runner Configuration
+    // ====================
+    //
+    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
+    // on a remote machine).
+    runner: 'local',
+    gptDriverApiKey: "<API_KEY>",
+    //
+    // ==================
+    // Specify Test Files
+    // ==================
+    // Define which test specs should run. The pattern is relative to the directory
+    // from which `wdio` was called. Notice that, if you are calling `wdio` from an
+    // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
+    // directory is where your package.json resides, so `wdio` will be called from there.
+    //
+    specs: [
+        './test/**/*.spec.js'
+    ],
+    // Patterns to exclude.
+    exclude: [
+        './test/**/*.page.js'
+    ],
+    // ============
+    // Capabilities
+    // ============
+    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
+    // time. Depending on the number of capabilities, WebdriverIO launches several test
+    // sessions. Within your capabilities you can overwrite the spec and exclude options in
+    // order to group specific specs to a specific capability.
+    //
+    // First, you can define how many instances should be started at the same time. Let's
+    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
+    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
+    // files and you set maxInstances to 10, all spec files will get tested at the same time
+    // and 30 processes will get spawned. The property handles how many capabilities
+    // from the same test should run tests.
+    //
+    maxInstances: 10,
+    capabilities: [
+        {
+            platformName: 'Android',
+            'appium:automationName': 'UiAutomator2',
+        },
+    ],
+    //
+    // ===================
+    // Test Configurations
+    // ===================
+    // Define all options that are relevant for the WebdriverIO instance here
+    //
+    // Level of logging verbosity: trace | debug | info | warn | error | silent
+    logLevel: process.env.DEBUG ? 'info' : 'warn',
+    //
+    // Set specific log levels per logger
+    // loggers:
+    // - webdriver, webdriverio
+    // - @wdio/applitools-service, @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
+    // - @wdio/mocha-framework, @wdio/jasmine-framework
+    // - @wdio/local-runner, @wdio/lambda-runner
+    // - @wdio/sumologic-reporter
+    // - @wdio/cli, @wdio/config, @wdio/sync, @wdio/utils
+    // Level of logging verbosity: trace | debug | info | warn | error | silent
+    // logLevels: {
+    //     webdriver: 'info',
+    //     '@wdio/applitools-service': 'info'
+    // },
+    //
+    // If you only want to run your tests until a specific amount of tests have failed use
+    // bail (default is 0 - don't bail, run all tests).
+    bail: 0,
+    //
+    // Default timeout for all waitFor* commands.
+    waitforTimeout: elementTimeout,
+    //
+    // Default timeout in milliseconds for request
+    // if browser driver or grid doesn't send response
+    connectionRetryTimeout: 90000,
+    //
+    // Default request retries count
+    connectionRetryCount: 3,
+    //
+    // Framework you want to run your specs with.
+    // The following are supported: Mocha, Jasmine, and Cucumber
+    // see also: https://webdriver.io/docs/frameworks.html
+    //
+    // Make sure you have the wdio adapter package for the specific framework installed
+    // before running any tests.
+    framework: 'mocha',
+    //
+    // Options to be passed to Mocha.
+    // See the full list at http://mochajs.org/
+    mochaOpts: {
+        ui: "bdd",
+        timeout: 60000 * 10,
+    },
+    // Test reporter for stdout.
+    // The only one supported by default is 'dot'
+    // see also: https://webdriver.io/docs/dot-reporter.html
+    reporters: [
+        'spec',
+        ['allure', {
+            outputDir: 'report/allure-results',
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: false,
+        }],
+    ],
+    //
+    // =====
+    // Hooks
+    // =====
+    // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
+    // it and to build services around it. You can either apply a single function or an array of
+    // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
+    // resolved to continue.
+    /**
+     * Gets executed before test execution begins. At this point you can access to all global
+     * variables like `browser`. It is the perfect place to define custom commands.
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that are to be run
+     */
+    before: function (capabilities, specs) {
+        global.allure = allure;
+        // global.chai = chai;
+    },
+    /**
+     * Hook that gets executed before the suite starts
+     * @param {Object} suite suite details
+     */
+    beforeSuite: function (suite) {
+        allure.addFeature(suite.name);
+    },
+    /**
+     * Function to be executed before a test (in Mocha/Jasmine) starts.
+     */
+    beforeTest: function (test, context) {
+        // allure.addEnvironment("BROWSER", browser.capabilities.browserName);
+        // allure.addEnvironment("BROWSER_VERSION", browser.capabilities.version);
+        // allure.addEnvironment("PLATFORM", browser.capabilities.platform);
+
+    },
+};
